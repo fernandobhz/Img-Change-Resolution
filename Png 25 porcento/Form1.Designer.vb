6d959d2afb2d127a9b70470bb7b35ce3579a8269
Fernando Reis Guimarães <fernandobhz@gmail.com>
2020-07-25 19:08:25 -0300

    old project migrated

---
+++ Png 25 porcento/Form1.Designer.vb
@@ -0,0 +1,77 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnReduzir = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnReduzir
+        '
+        Me.btnReduzir.Location = New System.Drawing.Point(402, 104)
+        Me.btnReduzir.Name = "btnReduzir"
+        Me.btnReduzir.Size = New System.Drawing.Size(120, 23)
+        Me.btnReduzir.TabIndex = 0
+        Me.btnReduzir.Text = "Mudar Resolução"
+        Me.btnReduzir.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(63, 110)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(217, 20)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "c:\imagem.png"
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(301, 107)
+        Me.NumericUpDown1.Maximum = New Decimal(New Integer() {1000, 0, 0, 0})
+        Me.NumericUpDown1.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(81, 20)
+        Me.NumericUpDown1.TabIndex = 2
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {25, 0, 0, 0})
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(534, 348)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btnReduzir)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnReduzir As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents NumericUpDown1 As System.Windows.Forms.NumericUpDown
+
+End Class
---
+++ Png 25 porcento/Form1.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Drawing.Imaging
+Imports System.IO.Path
+
+Public Class Form1
+    Private Args() As String = System.Environment.GetCommandLineArgs()
+
+    Public Sub ChangeResolution(ByVal InputPath As String, ByVal OutputPath As String, ByVal Fator As Decimal)
+
+        Dim img As New Bitmap(InputPath)
+        img.GetThumbnailImage(img.Width * Fator, img.Height * Fator, Nothing, IntPtr.Zero).Save(OutputPath, img.RawFormat)
+
+    End Sub
+
+    Sub AbrirArquivoProgramaPadrao(ByVal Arquivo As String)
+        Dim p As New System.Diagnostics.Process
+        Dim s As New System.Diagnostics.ProcessStartInfo(Arquivo)
+        s.UseShellExecute = True
+        s.WindowStyle = ProcessWindowStyle.Normal
+        p.StartInfo = s
+        p.Start()
+    End Sub
+
+    Private Sub btnReduzir_Click(sender As System.Object, e As System.EventArgs) Handles btnReduzir.Click
+        Dim path As String = Me.TextBox1.Text
+
+        If Not IO.File.Exists(path) Then
+            MsgBox("Arquivo não existe", MsgBoxStyle.Critical)
+            Exit Sub
+        End If
+
+        Dim novoPath As String = path & "." & Me.NumericUpDown1.Value & "." & Microsoft.VisualBasic.Right(path, 3)
+
+        ChangeResolution(path, novoPath, Me.NumericUpDown1.Value / 100)
+
+        AbrirArquivoProgramaPadrao(novoPath)
+
+    End Sub
+
+End Class
